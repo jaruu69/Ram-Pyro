8178573d9223d00e63ec82d65ea2dca809fd2ac9
jaruu <158283099+zarszs@users.noreply.github.com>
2024-02-23 23:01:16 +0700

    help.py

--- rams/modules/help.py
+++ rams/modules/help.py
@@ -46,13 +46,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "Ram Pyro Modules"
+            ac.title = "Jar Pyro Modules"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •",
+                f"```{str(ac)}```\n• @JarSuprot >< @uTits •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -66,7 +66,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"《✧ {str(help_arg).upper()}✧》**\n\n"
             for x in commands:
                 this_command += f"**ᴄᴍᴅ:\n├⋟** `{str(x)}`\n**└⋟ Fungsi:** `{str(commands[x])}`\n\n"
-            this_command += "© @UserbotCh"
+            this_command += "© @JarSuprot"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -87,12 +87,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "Ram Pyro Modules"
+        ac.title = "Jar Pyro Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •"
+            message, f"```{str(ac)}```\n• @JarSuprot >< @uTits •"
         )
         await message.reply(
             f"**Usage**:`{cdm}help broadcast` **To View Module details**"
@@ -104,7 +104,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"《✧**Help For {str(help_arg).upper()}✧》**\n\n"
             for x in commands:
                 this_command += f"**ᴄᴍᴅ:\n├⋟** `{str(x)}`\n**└⋟ Fungsi:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezRam >< @UserbotCh"
+            this_command += "© @JarSuprot >< @uTits"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
